6f95da360f867ba6c142288faaeee46410b29f2e
neoalf <ultraneom@gmail.com>
2025-11-26 21:56:19 -0600

    HTTP en Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'LinguaGo-Mobile',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http'
+  }
 };
 
 export default config;
